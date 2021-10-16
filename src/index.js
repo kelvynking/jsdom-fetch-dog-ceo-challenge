be21baacce17733548b8c56b8a07bf2c3040ab2f
kelvynking <kelvyn.m.king@gmail.com>
2021-10-16 13:45:49 +1100

    Completed Challenge 1.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => addImages(json))
+})
+
+function addImages(images) {
+    let container = document.getElementById("dog-image-container");
+    images.message.forEach(element => {
+        let image = document.createElement("img")
+        image.src = element;
+        container.appendChild(image);
+    })
+}
