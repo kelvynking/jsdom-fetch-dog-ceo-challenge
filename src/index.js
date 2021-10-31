1bd996eaea095f32f501123dcee7f7b97517beab
kelvynking <kelvyn.m.king@gmail.com>
2021-10-31 15:16:56 +1100

    Completed Challenge 2.

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const breedsList = data => {
         const breedContainer = document.querySelector("#dog-breeds")
-        
-        for (const breed in data.message) {
+        let breeds = data.message 
+
+        for (const breed in breeds) {
             const list = document.createElement("li")
             list.innerText = breed
             breedContainer.appendChild(list)
@@ -27,65 +28,4 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(resp => resp.json())
     .then(breedsList)
     .catch(error => console.log(error))
-})
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     fetch("https://dog.ceo/api/breeds/image/random/4")
-//     .then(resp => resp.json())
-//     .then(json => addImages(json)),
-//     fetch("https://dog.ceo/api/breeds/list/all")
-//     .then(resp => resp.json())
-//     .then(json => addBreeds(json))
-// })
-
-// function addImages(images) {
-//     let container = document.getElementById("dog-image-container");
-//     images.message.forEach(element => {
-//         let image = document.createElement("img")
-//         image.src = element;
-//         container.appendChild(image);
-//     })
-// }
-
-// function addBreeds(breeds) {
-//     let ulist = document.getElementById("dog-breeds");
-//     breeds.message.forEach(breed => {
-//         let list = document.createElement("li");
-//         list.innerText = breed;
-//         ulist.appendChild(list);
-//     })
-// }
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(resp => resp.json())
-    .then(json => addImages(json))
-})
-
-function addImages(images) {
-    let container = document.getElementById("dog-image-container");
-    images.message.forEach(element => {
-        let image = document.createElement("img")
-        image.src = element;
-        container.appendChild(image);
-    })
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://dog.ceo/api/breeds/list/all")
-    .then(resp => resp.json())
-    .then(json => {console.log(json)
-        let breeds = json.message;
-        let ulist = document.getElementById("dog-breeds");
-        for (const breed in breeds) {
-            let list = document.createElement("li");
-            list.innerText = breed;
-            ulist.appendChild(list);
-        }
-    })
 })
